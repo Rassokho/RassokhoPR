76fc24531c2868606476caa065dae89e3c37c803
316de <316de@315-DE-11>
2023-07-04 09:53:40 +0300

    Добавьте файлы проекта.

---
+++ RassokhoPR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RassokhoPR
+{
+    internal class store
+    {
+        
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("name,number,");
+            Console.WriteLine("Name,Address,Products");
+            string s = Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
